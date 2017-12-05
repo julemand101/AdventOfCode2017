00cbaa18c3d6935fcd6b883c53bf83f6042d816b
Jacob Bang <julemand101@archlinux.dk>
2017-12-05 20:24:32 +0100

    Day05: Solved

---
+++ lib/day05.dart
@@ -0,0 +1,29 @@
+// --- Day 5: A Maze of Twisty Trampolines, All Alike ---
+// http://adventofcode.com/2017/day/5
+
+typedef int NextPositionCalculator(List<int> instructions, int position);
+
+int solveA(List<int> input) {
+  return _solve(
+      input, (List<int> instructions, int pos) => pos + (instructions[pos]++));
+}
+
+int solveB(List<int> input) {
+  return _solve(
+      input,
+      (List<int> instructions, int pos) =>
+          pos +
+          ((instructions[pos] < 3)
+              ? instructions[pos]++
+              : instructions[pos]--));
+}
+
+int _solve(List<int> instructions, NextPositionCalculator nextPosition) {
+  int count = 0;
+
+  for (int pos = 0; (pos >= 0 && pos < instructions.length); count++) {
+    pos = nextPosition(instructions, pos);
+  }
+
+  return count;
+}
---
+++ test/day05_test.dart
@@ -0,0 +1,42 @@
+// --- Day 5: A Maze of Twisty Trampolines, All Alike ---
+// http://adventofcode.com/2017/day/5
+
+import 'dart:io';
+import 'package:test/test.dart';
+import 'package:AdventOfCode2017/day05.dart';
+
+const String DATA_FILE_PATH = "test/data/day05.txt";
+
+void main() {
+  group("Part One", () {
+    test('Example 1', () {
+      expect(solveA([0, 3, 0, 1, -3]), equals(5));
+    });
+    test('Solution', () {
+      File f = new File(DATA_FILE_PATH);
+      expect(
+          solveA(f
+              .readAsLinesSync()
+              .where((line) => line.trim().isNotEmpty)
+              .map((line) => int.parse(line))
+              .toList(growable: false)),
+          equals(372671));
+    });
+  });
+
+  group("Part Two", () {
+    test('Example 1', () {
+      expect(solveB([0, 3, 0, 1, -3]), equals(10));
+    });
+    test('Solution', () {
+      File f = new File(DATA_FILE_PATH);
+      expect(
+          solveB(f
+              .readAsLinesSync()
+              .where((line) => line.trim().isNotEmpty)
+              .map((line) => int.parse(line))
+              .toList(growable: false)),
+          equals(25608480));
+    });
+  });
+}
