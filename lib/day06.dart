9b2f7ea7c43f995f2648ebded5eac6e339dc1501
Jacob Bang <julemand101@archlinux.dk>
2017-12-06 18:09:11 +0100

    Day06: Solved

---
+++ lib/day06.dart
@@ -0,0 +1,66 @@
+// --- Day 6: Memory Reallocation ---
+// http://adventofcode.com/2017/day/6
+
+import 'package:collection/collection.dart';
+
+final ListEquality listEquality = new ListEquality();
+
+int solveA(List<int> banks) {
+  return _solve(banks).length;
+}
+
+int solveB(List<int> banks) {
+  List<List<int>> history = _solve(banks);
+
+  for (int i = 0; i < history.length; i++) {
+    if (listEquality.equals(history[i], history.last)) {
+      return history.length - i - 1;
+    }
+  }
+
+  throw("Something's wrong with the loop!");
+}
+
+List<List<int>> _solve(List<int> banks) {
+  List<List<int>> history = new List();
+  List<int> next = banks;
+
+  while (true) {
+    next = _nextBanksState(next);
+
+    if (history.any((List<int> prev) => listEquality.equals(next, prev))) {
+      history.add(next);
+      return history;
+    } else {
+      history.add(next);
+    }
+  }
+}
+
+List<int> _nextBanksState(List<int> banks) {
+  List<int> nextBanksState = new List.from(banks, growable: false);
+  int bankToEmpty = _findNextBankToEmpty(nextBanksState);
+  int blocks = nextBanksState[bankToEmpty];
+
+  nextBanksState[bankToEmpty] = 0;
+
+  for (int i = bankToEmpty + 1; blocks > 0; i++, blocks--) {
+    nextBanksState[i % nextBanksState.length]++;
+  }
+
+  return nextBanksState;
+}
+
+int _findNextBankToEmpty(List<int> banks) {
+  int bigPos = 0;
+  int bigValue = banks[0];
+
+  for (int i = 1; i < banks.length; i++) {
+    if (banks[i] > bigValue) {
+      bigPos = i;
+      bigValue = banks[i];
+    }
+  }
+
+  return bigPos;
+}
---
+++ test/day06_test.dart
@@ -0,0 +1,27 @@
+// --- Day 6: Memory Reallocation ---
+// http://adventofcode.com/2017/day/6
+
+import 'package:test/test.dart';
+import 'package:AdventOfCode2017/day06.dart';
+
+void main() {
+  group("Part One", () {
+    test('Example 1', () {
+      expect(solveA([0, 2, 7, 0]), equals(5));
+    });
+    test('Solution', () {
+      expect(solveA([0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11]),
+          equals(7864));
+    });
+  });
+
+  group("Part Two", () {
+    test('Example 1', () {
+      expect(solveB([0, 2, 7, 0]), equals(4));
+    });
+    test('Solution', () {
+      expect(solveB([0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11]),
+          equals(1695));
+    });
+  });
+}
