8e12c3c988f0d14c339af956c58dcd7749038803
Jacob Bang <julemand101@gmail.com>
2017-12-10 20:23:46 +0100

    Day10: Prepare

---
+++ lib/day10.dart
@@ -0,0 +1,10 @@
+// --- Day 10: Knot Hash ---
+// https://adventofcode.com/2017/day/10
+
+int solveA(List<int> lengths, List<int> circularList) {
+  int currentPosition = 0;
+  int skipSize = 0;
+  
+
+  print(circularList);
+}
---
+++ test/day10_test.dart
@@ -0,0 +1,45 @@
+// --- Day 10: Knot Hash ---
+// https://adventofcode.com/2017/day/10
+
+import 'package:test/test.dart';
+import 'package:AdventOfCode2017/day10.dart';
+
+void main() {
+  group("Part One", () {
+    test('Example 1', () {
+      expect(solveA([3, 4, 1, 5], new List.generate(5, (x) => x)), equals(12));
+    });
+    test('Solution', () {
+      expect(
+          solveA([
+            157,
+            222,
+            1,
+            2,
+            177,
+            254,
+            0,
+            228,
+            159,
+            140,
+            249,
+            187,
+            255,
+            51,
+            76,
+            30
+          ], new List.generate(256, (x) => x)),
+          equals(-1));
+    });
+  });
+
+//  group("Part Two", () {
+//    test('Example 1', () {
+//      expect(solveB([0, 2, 7, 0]), equals(4));
+//    });
+//    test('Solution', () {
+//      expect(solveB([0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11]),
+//          equals(1695));
+//    });
+//  });
+}
