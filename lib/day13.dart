2cb3726dfd7d95718149768928ecaa354b6d8103
Jacob Bang <julemand101@archlinux.dk>
2017-12-17 16:51:26 +0100

    Day13a: Solved

---
+++ lib/day13.dart
@@ -0,0 +1,55 @@
+// --- Day 13: Packet Scanners ---
+// https://adventofcode.com/2017/day/13
+
+import 'dart:math' as math;
+
+int solveA(Iterable<String> inputs) {
+  Map<int, int> map = _parse(inputs);
+  int max = map.keys.reduce(math.max);
+  int score = 0;
+
+  for (int depth = 0; depth <= max; depth++) {
+    if (map.containsKey(depth)) {
+      int range = map[depth];
+
+      if (_move(depth, range) == 0) {
+        score += (depth * range);
+      }
+    }
+  }
+
+  return score;
+}
+
+Map<int, int> _parse(Iterable<String> inputs) {
+  Map<int, int> map = new Map();
+
+  for (String input in inputs) {
+    var parts = input.split(": "); // e.g. "0: 3"
+    int depth = int.parse(parts[0]);
+    int range = int.parse(parts[1]);
+
+    map[depth] = range;
+  }
+
+  return map;
+}
+
+int _move(int time, int range) {
+  int pos = 0;
+  bool back = false;
+
+  for (int i = 0; i < time; i++) {
+    if (back) {
+      if (--pos == 0) {
+        back = false;
+      }
+    } else {
+      if (++pos == (range - 1)) {
+        back = true;
+      }
+    }
+  }
+
+  return pos;
+}
---
+++ test/day13_test.dart
@@ -0,0 +1,36 @@
+// --- Day 13: Packet Scanners ---
+// https://adventofcode.com/2017/day/13
+
+import 'dart:io';
+import 'package:test/test.dart';
+import 'package:AdventOfCode2017/day13.dart';
+
+const String DATA_FILE_PATH = "test/data/day13.txt";
+
+final List<String> EXAMPLE = ["0: 3", "1: 2", "4: 4", "6: 4"];
+
+void main() {
+  group("Part One", () {
+    test('Example 1', () {
+      expect(solveA(EXAMPLE), equals(24));
+    });
+    test('Solution', () {
+      File f = new File(DATA_FILE_PATH);
+      expect(
+          solveA(f.readAsLinesSync().where((line) => line.trim().isNotEmpty)),
+          equals(2160));
+    });
+  });
+
+//  group("Part Two", () {
+//    test('Example 1', () {
+//      expect(solveB(EXAMPLE), equals(2));
+//    });
+//    test('Solution', () {
+//      File f = new File(DATA_FILE_PATH);
+//      expect(
+//          solveB(f.readAsLinesSync().where((line) => line.trim().isNotEmpty)),
+//          equals(202));
+//    });
+//  });
+}
