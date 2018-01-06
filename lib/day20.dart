8ea1b2921436598ff6242fb0ab3e7381f9b0348d
Jacob Bang <julemand101@archlinux.dk>
2018-01-06 14:11:23 +0100

    Day20b: Solved
    Also made a lot of refactor of the part a code.

--- lib/day20.dart
+++ lib/day20.dart
@@ -1,32 +1,85 @@
 // --- Day 20: Particle Swarm ---
 // https://adventofcode.com/2017/day/20
 
-import 'dart:typed_data';
+class Coordinate {
+  int x, y, z;
+  Coordinate(this.x, this.y, this.z);
+
+  add(Coordinate c) {
+    x += c.x;
+    y += c.y;
+    z += c.z;
+  }
+}
 
 class Particle {
   final int id;
-  Int32x4 _position;
-  Int32x4 _velocity;
-  Int32x4 _acceleration;
+  Coordinate _position;
+  Coordinate _velocity;
+  Coordinate _acceleration;
 
   Particle(this.id, this._position, this._velocity, this._acceleration);
 
-  Int32x4 get position => _position;
-  Int32x4 get velocity => _velocity;
-  Int32x4 get acceleration => _acceleration;
-
-  int get accelerationAsManhattanDistance =>
-      _acceleration.x.abs() + _acceleration.y.abs() + _acceleration.z.abs();
+  Coordinate get position => _position;
+  Coordinate get velocity => _velocity;
+  Coordinate get acceleration => _acceleration;
 
   void tick() {
-    _velocity += _acceleration;
-    _position += _velocity;
+    _velocity.add(_acceleration);
+    _position.add(_velocity);
+  }
+
+  String toString() {
+    return "$id pos=$_position}";
+  }
+
+  bool equalPosition(Particle p) {
+    return (_position.x == p._position.x &&
+        _position.y == p._position.y &&
+        _position.z == p._position.z);
   }
 }
 
 int solveA(List<String> input) {
+  return parse(input).reduce((p1, p2) {
+    return manhattanDistanceFromZero(p1.acceleration) <
+            manhattanDistanceFromZero(p2.acceleration)
+        ? p1
+        : p2;
+  }).id;
+}
+
+int solveB(List<String> input) {
+  List<Particle> particles = parse(input);
+
+  // This solution is kind of a hack because the loop count is found after the
+  // solution was found (by using 1000 as loop count). 40 is the minimum number
+  // of loops we need to find the solution and is hardcoded to make the tests
+  // faster.
+  //
+  // The best solution whould be to found the right time when we should stop
+  // the loop. Maybe some day I go back and fix this. :)
+  for (int i = 0; i < 40; i++) {
+    List<Particle> copyList = particles.toList();
+
+    for (Particle p1 in copyList) {
+      for (Particle p2 in copyList) {
+        if (p1.id != p2.id && p1.equalPosition(p2)) {
+          particles.remove(p1);
+          particles.remove(p2);
+        }
+      }
+    }
+
+    particles.forEach((x) => x.tick());
+  }
+
+  return particles.length;
+}
+
+List<Particle> parse(List<String> input) {
   int id = 0;
-  List<Particle> particles = input.map((String line) {
+  return input.map((String line) {
     var parts = line.split(", ");
     var position = parseCoordinate(parts[0]);
     var velocity = parseCoordinate(parts[1]);
@@ -34,18 +87,14 @@ int solveA(List<String> input) {
 
     return new Particle(id++, position, velocity, acceleration);
   }).toList();
-
-  return particles.reduce((p1, p2) {
-    return p1.accelerationAsManhattanDistance <
-            p2.accelerationAsManhattanDistance
-        ? p1
-        : p2;
-  }).id;
 }
 
 // p=< 3,0,0>
-Int32x4 parseCoordinate(String input) {
+Coordinate parseCoordinate(String input) {
   var parts = input.substring(3, input.length - 1).trim().split(",");
-  return new Int32x4(
-      int.parse(parts[0]), int.parse(parts[1]), int.parse(parts[2]), 0);
+  return new Coordinate(
+      int.parse(parts[0]), int.parse(parts[1]), int.parse(parts[2]));
 }
+
+int manhattanDistanceFromZero(Coordinate coordinate) =>
+    coordinate.x.abs() + coordinate.y.abs() + coordinate.z.abs();
--- test/day20_test.dart
+++ test/day20_test.dart
@@ -7,15 +7,22 @@ import 'package:AdventOfCode2017/day20.dart';
 
 const String DATA_FILE_PATH = "test/data/day20.txt";
 
-final List<String> EXAMPLE = [
+final List<String> EXAMPLE_PART_ONE = [
   "p=< 3,0,0>, v=< 2,0,0>, a=<-1,0,0>",
   "p=< 4,0,0>, v=< 0,0,0>, a=<-2,0,0>"
+];
+
+final List<String> EXAMPLE_PART_TWO = [
+  "p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>",
+  "p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>",
+  "p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>",
+  "p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>"
 ];
 
 void main() {
   group("Part One", () {
     test('Example 1', () {
-      expect(solveA(EXAMPLE), equals(0));
+      expect(solveA(EXAMPLE_PART_ONE), equals(0));
     });
     test('Solution', () {
       expect(
@@ -24,6 +31,20 @@ void main() {
               .where((line) => line.trim().isNotEmpty)
               .toList(growable: false)),
           equals(170));
+    });
+  });
+
+  group("Part Two", () {
+    test('Example 1', () {
+      expect(solveB(EXAMPLE_PART_TWO), equals(1));
+    });
+    test('Solution', () {
+      expect(
+          solveB(new File(DATA_FILE_PATH)
+              .readAsLinesSync()
+              .where((line) => line.trim().isNotEmpty)
+              .toList(growable: false)),
+          equals(571));
     });
   });
 }
